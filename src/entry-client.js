08676dca03d01c4cdaf1dd1adb829f28dabdfaf8
zwlijun <zwlijun@hotmail.com>
2019-03-20 18:14:37 +0800

    调整client端配置

--- src/entry-client.js
+++ src/entry-client.js
@@ -23,6 +23,21 @@ const { app, router, store } = createVueApp()
 // the state is determined during SSR and inlined in the page markup.
 if (window.__INITIAL_STATE__) {
   store.replaceState(window.__INITIAL_STATE__)
+}else{
+  Vue.mixin({
+    beforeMount () {
+      const { asyncData } = this.$options
+      if (asyncData) {
+        // 将获取数据操作分配给 promise
+        // 以便在组件中，我们可以在数据准备就绪后
+        // 通过运行 `this.dataPromise.then(...)` 来执行其他任务
+        this.dataPromise = asyncData({
+          store: this.$store,
+          route: this.$route
+        })
+      }
+    }
+  })
 }
 
 router.onReady(() => {
