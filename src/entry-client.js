925897fa39f2fa773758ee1f1bd605e2ffa247e0
zwlijun <zwlijun@hotmail.com>
2019-03-20 18:10:36 +0800

    调整client端配置

--- src/entry-client.js
+++ src/entry-client.js
@@ -2,42 +2,27 @@ import Vue from 'vue'
 import 'es6-promise/auto'
 import { createVueApp } from './app'
 
+// a global mixin that calls `asyncData` when a route component's params change
+Vue.mixin({
+  beforeRouteUpdate (to, from, next) {
+    const { asyncData } = this.$options
+    if (asyncData) {
+      asyncData({
+        store: this.$store,
+        route: to
+      }).then(next).catch(next)
+    } else {
+      next()
+    }
+  }
+})
+
 const { app, router, store } = createVueApp()
 
 // prime the store with server-initialized state.
 // the state is determined during SSR and inlined in the page markup.
 if (window.__INITIAL_STATE__) {
-  // a global mixin that calls `asyncData` when a route component's params change
-  Vue.mixin({
-    beforeRouteUpdate (to, from, next) {
-      const { asyncData } = this.$options
-      if (asyncData) {
-        asyncData({
-          store: this.$store,
-          route: to
-        }).then(next).catch(next)
-      } else {
-        next()
-      }
-    }
-  })
-  
   store.replaceState(window.__INITIAL_STATE__)
-}else{
-  Vue.mixin({
-    beforeMount () {
-      const { asyncData } = this.$options
-      if (asyncData) {
-        // 将获取数据操作分配给 promise
-        // 以便在组件中，我们可以在数据准备就绪后
-        // 通过运行 `this.dataPromise.then(...)` 来执行其他任务
-        this.dataPromise = asyncData({
-          store: this.$store,
-          route: this.$route
-        })
-      }
-    }
-  })
 }
 
 router.onReady(() => {
