332d4f912c0ecd324873577512bceac5b2e0f058
zwlijun <zwlijun@gmail.com>
2019-03-27 18:45:55 +0800

    增加js模块

--- src/lib/utils/equals.js
+++ src/lib/utils/equals.js
@@ -12,112 +12,112 @@
 'use strict';
 
 function dataType(obj){
-	let str = Object.prototype.toString.call(obj);
-	let type = (str.substring(str.indexOf(" ") + 1, str.indexOf("]"))).toLowerCase();
+    let str = Object.prototype.toString.call(obj);
+    let type = (str.substring(str.indexOf(" ") + 1, str.indexOf("]"))).toLowerCase();
 
-	return type;
+    return type;
 }
 
 function isHTMLElement(obj){
-	let type = dataType(obj);
-	let pattern = /^HTML[a-z]+Element$/i;
+    let type = dataType(obj);
+    let pattern = /^HTML[a-z]+Element$/i;
 
-	if(!pattern.test(type)){
-		return false;
-	}
+    if(!pattern.test(type)){
+        return false;
+    }
 
-	if(obj.nodeType && 1 === obj.nodeType && obj.outerHTML){
-		return true;
-	}
+    if(obj.nodeType && 1 === obj.nodeType && obj.outerHTML){
+        return true;
+    }
 
-	return false;
+    return false;
 }
 
 function str(obj){
-	return "" + obj;
+    return "" + obj;
 }
 
 function bool(obj){
-	return new Boolean(obj).valueOf();
+    return new Boolean(obj).valueOf();
 }
 
 function num(obj){
-	return new Number(obj).valueOf();
+    return new Number(obj).valueOf();
 }
 
 function func(obj){
-	let str = obj.toString();
+    let str = obj.toString();
 
-	str = str.replace(/^([\t\r\n ]+)|([\t\r\n ]+)$/mg, "");
+    str = str.replace(/^([\t\r\n ]+)|([\t\r\n ]+)$/mg, "");
 
-	return str;
+    return str;
 }
 
 function arr(o0, o1){
-	if(o0.length !== o1.length){
-		return false;
-	}
+    if(o0.length !== o1.length){
+        return false;
+    }
 
-	let result = true;
-	for(let i = 0, size = o0.length; i < size; i++){
-		if(!equals(o0[i], o1[i])){
-			result = false;
-			break;
-		}
-	}
+    let result = true;
+    for(let i = 0, size = o0.length; i < size; i++){
+        if(!equals(o0[i], o1[i])){
+            result = false;
+            break;
+        }
+    }
 
-	return result;
+    return result;
 }
 
 function obj(o0, o1){
-	if(!arr(Object.keys(o0), Object.keys(o1))){
-		return false;
-	}
+    if(!arr(Object.keys(o0), Object.keys(o1))){
+        return false;
+    }
 
-	if(!arr(Object.entries(o0), Object.entries(o1))){
-		return false;
-	}
+    if(!arr(Object.entries(o0), Object.entries(o1))){
+        return false;
+    }
 
-	return true;
+    return true;
 }
 
 function equals(obj1, obj2){
-	if(obj1 === obj2){
-		return true;
-	}
+    if(obj1 === obj2){
+        return true;
+    }
 
-	let type1 = dataType(obj1);
-	let type2 = dataType(obj2);
+    let type1 = dataType(obj1);
+    let type2 = dataType(obj2);
 
-	if(type1 !== type2){
-		return false;
-	}
+    if(type1 !== type2){
+        return false;
+    }
 
-	if(isHTMLElement(obj1)){
-		type1 = "element";
-	}
+    if(isHTMLElement(obj1)){
+        type1 = "element";
+    }
 
-	switch(type1){
-		case "regexp":
-		case "string":
-			return str(obj1) === str(obj2);
-		case "boolean":
-			return bool(obj1) === bool(obj2);
-		case "number":
-			return num(obj1) === num(obj2);
-		case "date":
-			return +obj1 === +obj2;
-		case "element":
-			return obj1.outerHTML === obj2.outerHTML;
-		case "function":
-			return func(obj1) === func(obj2);
-		case "array":
-			return arr(obj1, obj2);
-		case "object":
-			return obj(obj1, obj2);
-	}
+    switch(type1){
+        case "regexp":
+        case "string":
+            return str(obj1) === str(obj2);
+        case "boolean":
+            return bool(obj1) === bool(obj2);
+        case "number":
+            return num(obj1) === num(obj2);
+        case "date":
+            return +obj1 === +obj2;
+        case "element":
+            return obj1.outerHTML === obj2.outerHTML;
+        case "function":
+            return func(obj1) === func(obj2);
+        case "array":
+            return arr(obj1, obj2);
+        case "object":
+            return obj(obj1, obj2);
+    }
 
-	return false;
+    return false;
 }
 
 
--- src/lib/utils/listener.js
+++ src/lib/utils/listener.js
@@ -14,81 +14,163 @@
 const equals = require("./equals");
 
 class Listener{
-	_handlers = {};
+    _handlers = {};
 
-	constructor(handlers){
-		this._handlers = handlers || {};
-	}
+    constructor(handlers){
+        this._handlers = handlers || {};
+    }
 
-	/**
-	 * 获取handlers对象
-	 * @return {[type]} [description]
-	 */
-	public get handlers(){
-		return this._handlers;
-	}
+    /**
+     * 获取handlers对象
+     * @return {[type]} [description]
+     */
+    public get handlers(){
+        return this._handlers;
+    }
 
-	createHandler(handler){
-		let _handler = null;
+    /**
+     * 创建一个合法的handler
+     * @param  {[type]} handler [description]
+     * @return {[type]}         [description]
+     */
+    createHandler(handler){
+        let _handler = null;
 
-		if(Object.isFrozen(handler)){
-			return _handler;
-		}
+        if(Object.isFrozen(handler)){
+            return _handler;
+        }
 
-		let def = {
-			"callback": null,
-			"args": [],
-			"context": null,
-			"returnValue": false
-		};
+        let def = {
+            "callback": null,
+            "args": [],
+            "context": null
+        };
 
-		try{
-			Object.seal(def);
-			Object.assign(def, handler);
+        try{
+            Object.seal(def);
+            Object.assign(def, handler);
 
-			_handler = def;
-		}catch(e){
-			_handler = null;
-		}
+            _handler = def;
+        }catch(e){
+            _handler = null;
+        }
 
-		return _handler;
-	}
+        return _handler;
+    }
 
+    /**
+     * 检测是否存在
+     * @param  {[type]} key     [description]
+     * @param  {[type]} handler [description]
+     * @return {[type]}         [description]
+     */
+    existed(key, handler){
+        const _handler = this.createHandler(handler);
 
-	existed(key, handler){
-		const _handler = this.createHandler(handler);
+        if(null == _handler){
+            return true;
+        }
 
-		if(null == _handler){
-			return true;
-		}
+        const conf = this.handlers;
+        const items = conf["on" + key];
 
-		const conf = this.handlers;
-		const items = conf["on" + key];
+        if(!items){
+            this._handlers["on" + key] = [];
+            return false;
+        }
 
-		if(!items){
-			this._handlers["on" + key] = [];
-			return false;
-		}
+        const size = items.length;
+        for(let i = 0; i < size; i++){
+            if(equals(_handler, items[i])){
+                return true;
+            }
+        }
 
-		const size = items.length;
-		for(let i = 0; i < size; i++){
-			if(equals(_handler, items[i])){
-				return true;
-			}
-		}
+        return false;
+    }
 
-		return false;
-	}
+    /**
+     * 添加一个回调句柄
+     * @param {[type]} eventType [description]
+     * @param {[type]} handler   [description]
+     */
+    add(eventType, handler){
+        const key = "on" + eventType;
+        const conf = this.handlers;
+        const _handler = null;
 
-	add(eventType, handler){
-		const key = "on" + eventType;
-		const conf = this.handlers;
-		const _handler = null;
+        if(null !== (_handler = this.createHandler(handler)) && !this.existed(eventType, handler)){
+            conf[key].push(handler);
 
-		if(null !== (_handler = this.createHandler(handler)) && !this.existed(eventType, handler)){
-			conf[key].push(handler);
+            this._handlers[key] = conf[key];
+        }
+    }
 
-			this._handlers[key] = conf[key];
-		}
-	}
+    /**
+     * 移除所有handler或指定的handler
+     * @param  {[type]} eventType [description]
+     * @param  {[type]} handler   [description]
+     * @return {[type]}           [description]
+     */
+    remove(eventType, handler){
+        const key = "on" + eventType;
+        const conf = this.handlers;
+        const _handler = this.createHandler(handler);
+
+        if(key in conf){
+            if(!_handler){
+                delete this._handlers[key];
+                this._handlers[key] = null;
+            }else{
+                const items = conf[key] || [];
+                const size = items.length;
+                for(let i = 0; i < size; i++){
+                    if(equals(_handler, items[i])){
+                        this._handlers[key].splice(i, 1);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * 执行回调
+     * @param  {[type]} handler [description]
+     * @param  {[type]} args    [description]
+     * @return {[type]}         [description]
+     */
+    execHandler(handler, args){
+        const _args = [].concat(args || []);
+        const _handler = this.createHandler(handler);
+
+        if(_handler){
+            const $callback = _handler.callback || null;
+            const $args = _args.concat(_handler.args || []);
+            const $context = _handler.context || null;
+
+            if($callback && $callback.apply){
+                return $callback.apply($context, $args);
+            }
+        }
+
+        return undefined;
+    }
+
+    /**
+     * 执行所有注册的回调
+     * @param  {[type]} eventType [description]
+     * @param  {[type]} args      [description]
+     * @return {[type]}           [description]
+     */
+    exec(eventType, args){
+        const key = "on" + eventType;
+        const conf = this.handlers;
+        const items = conf[key] || [];
+        const size = items.length;
+
+        for(let i = 0; i < size; i++){
+            this.execHandler(items[i], args)
+        }
+    }
 }
